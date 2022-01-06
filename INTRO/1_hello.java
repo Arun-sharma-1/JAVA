62a569273906cc852b6b30274c6d379d270ee3d2
Arun <arunmadhopur750@gmail.com>
2022-01-06 16:15:42 +0530

    first commit

---
+++ INTRO/1_hello.java
@@ -0,0 +1,59 @@
+package INTRO;
+
+/*
+class helloworld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello world !");
+        System.out.println("This is my first Java program");
+    }
+}
+*/
+
+// ========================================================================================
+
+/*
+class Experiment
+{
+    void show()
+    {
+        System.out.println("This is show ");
+    }
+    public static void main(String[] args) 
+    {
+        Experiment ref = new Experiment();
+        ref.show();
+        System.out.println("This is Expriment ");   
+    }
+}
+*/
+//========================================================================================
+
+/*
+class Experiment
+{
+    static void show()
+    {
+        System.out.println("This is show ");
+    }
+    public static void main(String[] args) 
+    {
+
+        show();
+        System.out.println("This is Expriment ");   
+    }
+}
+*/
+
+//========================================================================================
+
+class A
+{
+    //java 1_hello.java arun sharma 
+    public static void main(String[] args) 
+    {
+         
+        System.out.print(args[0]);   //arun
+    }
+}
---
+++ INTRO/2_primitive_datatype.java
@@ -0,0 +1,24 @@
+package INTRO;
+
+
+
+// import javax.print.event.PrintEvent;
+
+class Primitive
+{
+    public static void main(String[] args)
+    {
+        byte numberOfSeats=4;
+        short teachersSalary=10000;
+        char name='A';
+        double range=45.3;
+        float keep=45.3f;
+        System.out.println("no of seats " + numberOfSeats);
+        System.out.println(name);
+        System.out.println(range);
+        System.out.println(teachersSalary);
+        System.out.println(keep);
+
+    }
+    
+}
