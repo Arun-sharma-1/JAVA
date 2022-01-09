e2044841aa2cdd30e0c8196caa1f18bc4e9661f0
Arun <arunmadhopur750@gmail.com>
2022-01-09 09:18:19 +0530

    files added

---
+++ INTRO/7_typecasting.java
@@ -0,0 +1,27 @@
+package INTRO;
+
+// import java.lang.ProcessBuilder.Redirect.Type;
+
+class TYPECASTING
+{
+    public static void main(String[] args) {
+        int i=10;
+        double j = i*10;
+        System.out.println( ( (Object) i) .getClass().getSimpleName() );
+        System.out.println( ( (Object) j) .getClass().getSimpleName() );
+
+        double d= 89999999999l; // int can not store such long value so it will give the maxm value that it can store
+        int c = (int)d;
+        System.out.println(c);
+        
+       
+
+        int  b=130;
+        byte a  = (byte)b;
+        System.out.println(a);
+        
+        byte b1=0b1111111;
+        System.out.println(b1);
+
+    }
+}
